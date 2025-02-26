53a06e74175fec8ce1e360a083df5808dba54641
Guilherme <doesntscript@gmail.com>
2025-02-26 10:56:41 -0300

    (AuthPags): Navigating between pages with right way

--- content/app/page.js
+++ content/app/page.js
@@ -1,9 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to Introduce Show Cars!</h1>
       <p>🔥 Let&apos;s see the beuatiful Design cd ! 🔥</p>
+      <p> <Link href="/about" >About Us</Link></p>
     </main>
   );
 }
