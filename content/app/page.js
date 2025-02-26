e125cf8d3c46d1bbcd0f85205af3c767a3de951f
Guilherme <doesntscript@gmail.com>
2025-02-26 11:24:32 -0300

    (AuthPags): Reserved file names and custm components and organize

--- content/app/page.js
+++ content/app/page.js
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import Header from "@/components/header";
 
 export default function Home() {
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+      <Header />
       <h1>Welcome to Introduce Show Cars!</h1>
       <p>🔥 Let&apos;s see the beuatiful Design cd ! 🔥</p>
       <p> <Link href="/about" >About Us</Link></p>
